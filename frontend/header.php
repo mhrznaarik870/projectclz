20c1378e688867100a4c32e02199bf599cdea920
Aarik Mhrzn <mhrznaarik870@gmail.com>
2023-07-01 15:26:02 +0545

    Revert "revert"
    This reverts commit db71857f3798e4e62f54c79a856d167d9134a576.

--- frontend/header.php
+++ frontend/header.php
@@ -1,3 +1,53 @@
+<?php
+$showAlert = false;
+$showError = false;
+$errorMessage = "";
+
+require_once '../backend/partials/_dbconnect.php';
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = $_POST['name'];
+    $password = $_POST['password'];
+    $email = $_POST['email'];
+
+    if (isset($_POST['terms']) && $_POST['terms'] === "1") {
+        // Check if the username already exists
+        $checkQuery = "SELECT * FROM clients WHERE name='$name'";
+        $checkResult = mysqli_query($conn, $checkQuery);
+
+        if (mysqli_num_rows($checkResult) > 0) {
+            $showError = true; // Set the error flag if username exists
+            $errorMessage = "Username already exists. Please reset your password and try to login!";
+        } else {
+            // Check if the email already exists
+            $checkEmailQuery = "SELECT * FROM clients WHERE email='$email'";
+            $checkEmailResult = mysqli_query($conn, $checkEmailQuery);
+
+            if (mysqli_num_rows($checkEmailResult) > 0) {
+                $showError = true; // Set the error flag if email exists
+                $errorMessage = "Email already exists. Please try resetting your password!";
+            } else {
+                // Insert the new user into the database
+                $sql = "INSERT INTO `clients` (`name`, `password`, `email`, `Reg_date`) VALUES ('$name', '$password', '$email', current_timestamp())";
+                $result = mysqli_query($conn, $sql);
+
+                if ($result) {
+                    $showAlert = true; // Set the success flag if insertion is successful
+                } else {
+                    $showError = true; // Set the error flag if insertion fails
+                    $errorMessage = "Unable to create your account. Please try again later.";
+                }
+            }
+        }
+    } else {
+        // Checkbox not checked, show error message
+        $showError = true;
+        $errorMessage = "Please accept the terms and conditions.";
+    }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,7 +69,7 @@
 <body style="background-color: rgb(87, 80, 80)">
     <header>
         <!-- navigation bar -->
-        <nav class="navbar navbar-expand-lg" id="navbar">
+        <nav class="navbar navbar-expand-lg">
             <div class="container">
                 <a class="navbar-brands" href="./index.php">
                     <img class="logo" src="./assets/img/logo.png" alt="logo" />
@@ -58,6 +108,25 @@
             </div>
         </nav>
 
+
+        <?php if ($showAlert): ?>
+            <div class="show-alerts ">
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>SUCCESS!!</strong> Your account is created. Please verify your email-address by visiting your
+                    gmail account.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+        <?php elseif ($showError): ?>
+            <div class="show-alerts ">
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Error!!</strong>
+                    <?php echo $errorMessage; ?>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            </div>
+        <?php endif; ?>
+
         <!-- registration and login form -->
         <div class="big-wrapper">
 
@@ -74,10 +143,10 @@
                         <img src="./assets/img/logo.png" alt="logo" class="registerico" />
 
                         <!-- Form action post -->
-                        <form method="POST" action="login_register.php">
+                        <form action="/projectclz/frontend/index.php" method="POST">
                             <div class="input-box">
                                 <span class="icon"><ion-icon name="people"></ion-icon> </span>
-                                <input type="text" id="name" name="name" placeholder="Full Name" required>
+                                <input type="text" id="name" name="name" placeholder="Username" required>
                             </div>
                             <div class="input-box">
                                 <span class="icon"><ion-icon name="mail"></ion-icon> </span>
@@ -87,11 +156,9 @@
                                 <span class="icon"> <ion-icon name="lock-closed"></ion-icon></span>
                                 <input type="password" id="password" name="password" placeholder="Password" required>
                             </div>
-                            <div class="input-box">
-                                <span class="icon"> <ion-icon name="call"></ion-icon></span>
-                                <input type="phoneno" id="phoneno" name="phoneno" placeholder="Phone No." required>
-                            </div>
                             <div class="register-box ">
+                                <label><input type="checkbox" name="terms" value="1">
+                                    I agree to the <a href="#">terms & conditions</a> </label>
                                 <button type="submit" class="regbtn">Register</button>
                             </div>
                             <div class="login-register">
@@ -100,25 +167,32 @@
                         </form>
                     </div>
                 </div>
-
                 <!-- login-box -->
                 <div class="form-box login">
                     <h2>Login</h2>
                     <img src="./assets/img/logo.png" alt="logo" class="loginico" />
-                    <form method="POST" action="login_register.php">
+                    <form action="#">
                         <div class="input-box">
                             <span class="icon"><ion-icon name="mail"></ion-icon> </span>
-                            <input type="mail" placeholder="E-Mail " required>
-                            <div class="forget-password">
-                                <p> <a href="#" class="login-link">Reset password??</a></p>
-                            </div>
-                            <div class="remember-forget">
-                                <button type="submit" class="btn">Login</button>
+                            <input type="mail" placeholder="E-Mail Address" required>
+                            <!-- <label> E-Mail Address</label> -->
+                        </div>
+                        <div class="input-box">
+                            <span class="icon"> <ion-icon name="lock-closed"></ion-icon></span>
+                            <input type="password" placeholder="Password" required>
+                            <!-- <label> Password</label> -->
+                        </div>
 
-                            </div>
-                            <div class="login-register">
-                                <p>Don't have an account? <a href="#" class="register-link">Register</a></p>
-                            </div>
+                        <div class="forget-password">
+                            <p> <a href="#" class="login-link">Reset password??</a></p>
+                        </div>
+                        <div class="remember-forget">
+                            <button type="submit" class="btn">Login</button>
+
+                        </div>
+                        <div class="login-register">
+                            <p>Don't have an account? <a href="#" class="register-link">Register</a></p>
+                        </div>
                     </form>
                 </div>
             </div>
